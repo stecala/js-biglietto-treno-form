0035a050d1f898b8697a4b33ed8fae1a74cf9e8c
stecala <ste.cala123@gmail.com>
2022-06-15 15:46:37 +0200

    correzzione

--- script/script.js
+++ script/script.js
@@ -11,20 +11,24 @@ button.addEventListener('click', function () {
     console.log(userAge);
     const wagonRand = Math.floor((Math.random() * 9) + 1);
     const cpRand = Math.floor((Math.random() * 100000) + 1);
+    document.getElementById('cont-remove').classList.add(('d-none'));
+    document.getElementById('wrong').classList.add(('d-none'));
+    document.getElementById('ticket').classList.add(('d-none'));
     if ( typeof(userName) !== "string" || userName =='') {
         console.log({userName});
         document.getElementById('cont-remove').classList.remove(('d-none'));
+        document.getElementById('wrong').classList.remove(('d-none'));
         document.getElementById('wrong').innerHTML = '<h2>Nome inserito non valido</h2>';
     }
-    else {
-        if ((isNaN(userKm) == true) || (isNaN(userAge) == true) || (userAge <= 0) || (userKm <= 0)) {
+    else if ((isNaN(userKm) == true) || (isNaN(userAge) == true) || (userAge <= 0) || (userKm <= 0)) {
             document.getElementById('cont-remove').classList.remove(('d-none'));
+            document.getElementById('wrong').classList.remove(('d-none'));
             document.getElementById('wrong').innerHTML = '<h2>Devi inserire solo dei numeri e devono essere maggiori di zero nei form numerici</h2>';
         }
         else {
             // calcolo prezzo del biglietto
             const userPrice = (userKm * 0.26).toFixed(2);
-            document.getElementById('wrong').classList.add(('d-none'));
+            document.getElementById('ticket').classList.remove(('d-none'));
             // applico eventuali sconti
             if (userAge < 18) {
                 let priceDiscounted = (userPrice - ((userPrice * 15) / 100)).toFixed(2);
@@ -57,5 +61,4 @@ button.addEventListener('click', function () {
                 document.getElementById('cost').innerHTML = userPrice;
             }
         }
-    }
 })
