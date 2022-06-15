624acc4e7c0883ad1e20531274037ef126053f45
stecala <ste.cala123@gmail.com>
2022-06-15 15:32:35 +0200

    creazione quasi completa

--- script/script.js
+++ script/script.js
@@ -1,32 +1,61 @@
 const button = document.getElementById('button');
 
-button.addEventListener('click' , function(){
+button.addEventListener('click', function () {
     console.log(document.getElementById('km').value);
-    const userName=document.getElementById('name').value;
-    const userKm=parseInt(document.getElementById('km').value);
-    const userAge=parseInt(document.getElementById('age').value);
+    const userName = document.getElementById('name').value;
+    const userKm = parseInt(document.getElementById('km').value);
+    const userAge = parseInt(document.getElementById('age').value);
 
     console.log(userName);
     console.log(userKm);
     console.log(userAge);
-    if((isNaN(userKm) == true ) || (isNaN(userAge) == true) || (userAge <= 0) || (userKm <=0)){
-        document.getElementById('wrong').innerHTML='Devi inserire solo dei numeri e devono essere maggiori di zero';
+    const wagonRand = Math.floor((Math.random() * 9) + 1);
+    const cpRand = Math.floor((Math.random() * 100000) + 1);
+    if ( typeof(userName) !== "string" || userName =='') {
+        console.log({userName});
+        document.getElementById('cont-remove').classList.remove(('d-none'));
+        document.getElementById('wrong').innerHTML = '<h2>Nome inserito non valido</h2>';
     }
-    else{
-        // calcolo prezzo del biglietto
-        const userPrice = (userKm * 0.26).toFixed(2);
-        // applico eventuali sconti
-        if(userAge < 18){
-            let priceDiscounted = (userPrice - ((userPrice * 15) / 100)).toFixed(2);
+    else {
+        if ((isNaN(userKm) == true) || (isNaN(userAge) == true) || (userAge <= 0) || (userKm <= 0)) {
+            document.getElementById('cont-remove').classList.remove(('d-none'));
+            document.getElementById('wrong').innerHTML = '<h2>Devi inserire solo dei numeri e devono essere maggiori di zero nei form numerici</h2>';
         }
-        else if(userAge >= 65)
-        {
-            let priceDiscounted = (userPrice - ((userPrice * 35) / 100)).toFixed(2);
-        }
-        // prezzo senza agevolazioni
-        else{
-            
+        else {
+            // calcolo prezzo del biglietto
+            const userPrice = (userKm * 0.26).toFixed(2);
+            document.getElementById('wrong').classList.add(('d-none'));
+            // applico eventuali sconti
+            if (userAge < 18) {
+                let priceDiscounted = (userPrice - ((userPrice * 15) / 100)).toFixed(2);
+                document.getElementById('cont-remove').classList.remove(('d-none'));
+                document.getElementById('ticket').classList.remove(('d-none'));
+                document.getElementById('user-name').innerHTML = userName;
+                document.getElementById('sale').innerHTML = 'Ridotto under18';
+                document.getElementById('wagon').innerHTML = wagonRand;
+                document.getElementById('cp').innerHTML = cpRand;
+                document.getElementById('cost').innerHTML = priceDiscounted;
+            }
+            else if (userAge >= 65) {
+                let priceDiscounted = (userPrice - ((userPrice * 35) / 100)).toFixed(2);
+                document.getElementById('cont-remove').classList.remove(('d-none'));
+                document.getElementById('ticket').classList.remove(('d-none'));
+                document.getElementById('user-name').innerHTML = userName;
+                document.getElementById('sale').innerHTML = 'Ridotto Over 65';
+                document.getElementById('wagon').innerHTML = wagonRand;
+                document.getElementById('cp').innerHTML = cpRand;
+                document.getElementById('cost').innerHTML = priceDiscounted;
+            }
+            // prezzo senza agevolazioni
+            else {
+                document.getElementById('cont-remove').classList.remove(('d-none'));
+                document.getElementById('ticket').classList.remove(('d-none'));
+                document.getElementById('user-name').innerHTML = userName;
+                document.getElementById('sale').innerHTML = 'Nessuna agevolazione';
+                document.getElementById('wagon').innerHTML = wagonRand;
+                document.getElementById('cp').innerHTML = cpRand;
+                document.getElementById('cost').innerHTML = userPrice;
+            }
         }
     }
-    
 })
